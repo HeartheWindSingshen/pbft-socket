18dfa1ba835de2aa1ca5ac8939a50cc3304b040c
HeartheWindSingshen <02clannad@gmail.com>
2024-04-16 23:04:38 +0800

    前面得所有pbft实现都是有大问题的，现在已经改正 本次改正主要是修复投票问题，为稳定版本，之后就是view-change了

--- src/main/java/coms/pbft/mainTest/PbftNodeMain1.java
+++ src/main/java/coms/pbft/mainTest/PbftNodeMain1.java
@@ -8,6 +8,7 @@ public class PbftNodeMain1 {
     public static void main(String[] args) throws FileNotFoundException {
         PbftNode pbftNode2 = new PbftNode(1, "127.0.0.1", 9002, false);
         pbftNode2.start();
+
     }
 
 }
